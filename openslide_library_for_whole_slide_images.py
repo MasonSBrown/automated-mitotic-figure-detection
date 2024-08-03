3887f9e2df1983041963fddda0660ebb66fccd26
Mason Brown <masubrow@ucsc.edu>
2024-08-02 22:09:05 -0700

    Cleaning up more comments and adding import os and import openslide.deepzoom

--- openslide_library_for_whole_slide_images.py
+++ openslide_library_for_whole_slide_images.py
@@ -1,7 +1,7 @@
 #pip install openslide-python
 
-from openslide import open_slide
 import openslide
+from openslide import open_slide
 from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
